dab777dd41f1c9f3435f6026c1b045feb3a34c7c
José Márquez Doblas <34044037+IronSenior@users.noreply.github.com>
2019-10-11 18:41:34 +0200

    poi-id class and tests created (#8)

---
+++ src/poi/domain/model/poi-id.spec.ts
@@ -0,0 +1,12 @@
+import { PointOfInterestId } from "./poi-id";
+
+describe('UserId', () => {
+  const uuidA = '1061abe8-37e5-4623-8696-a9fd40797f73';
+  const uuidB = 'f75dde03-2c8b-4e5a-89a2-ad22d52c18cf';
+
+  it('should be an uuid v4', () => {
+    const vo = PointOfInterestId.fromString(uuidA);
+
+    expect(vo.value).toBe(uuidA);
+  });
+});
