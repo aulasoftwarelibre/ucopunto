306e961be339122139272e0d06a037282db636e5
Adoen Lunnae <unshekdelmonton@gmail.com>
2019-10-11 18:34:45 +0200

    Created RideRequestId VO

---
+++ src/ride-request/domain/model/ride-request-id.spec.ts
@@ -0,0 +1,12 @@
+import { RideRequestId } from "./ride-request-id";
+
+describe('RideRequestId', () => {
+  const uuidA = '1061abe8-37e5-4623-8696-a9fd40797f73';
+  const uuidB = 'f75dde03-2c8b-4e5a-89a2-ad22d52c18cf';
+
+  it('should be an uuid v4', () => {
+    const vo = RideRequestId.fromString(uuidA);
+
+    expect(vo.value).toBe(uuidA);
+  });
+});
