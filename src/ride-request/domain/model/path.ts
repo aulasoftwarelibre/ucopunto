bbd0275fec73685fa8e5458f3e6d12ce8e7bb807
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-10-18 19:22:35 +0200

    Added Path VO (#17)
    * Added Path VO

---
+++ src/ride-request/domain/model/path.ts
@@ -0,0 +1,31 @@
+import { ValueObject } from '../../../core/domain';
+import { PointOfInterestId } from '../../../poi/domain/model/poi-id';
+import { NumberOfPassengers } from './number-of-passengers';
+
+interface Props {
+  origin: PointOfInterestId;
+  arrivals: PointOfInterestId[];
+  numberOfPassengers: NumberOfPassengers;
+}
+
+export class Path extends ValueObject<Props> {
+  static with(
+    origin: PointOfInterestId,
+    arrivals: PointOfInterestId[],
+    numberOfPassengers: NumberOfPassengers,
+  ): Path {
+    return new this({ origin, arrivals, numberOfPassengers });
+  }
+
+  get origin(): PointOfInterestId {
+    return this.props.origin;
+  }
+
+  get arrivals(): PointOfInterestId[] {
+    return this.props.arrivals;
+  }
+
+  get numberOfPassengers(): NumberOfPassengers {
+    return this.props.numberOfPassengers;
+  }
+}
