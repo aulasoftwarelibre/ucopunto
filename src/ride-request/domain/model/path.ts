f56ad36ec01e8dffd214338656716f5f67625b25
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-10-18 19:16:51 +0200

    Path VO formatted

--- src/ride-request/domain/model/path.ts
+++ src/ride-request/domain/model/path.ts
@@ -1,31 +1,31 @@
-import { ValueObject } from "../../../core/domain";
-import { PointOfInterestId } from "../../../poi/domain/model/poi-id";
-import { NumberOfPassengers } from "./number-of-passengers";
+import { ValueObject } from '../../../core/domain';
+import { PointOfInterestId } from '../../../poi/domain/model/poi-id';
+import { NumberOfPassengers } from './number-of-passengers';
 
 interface Props {
-    origin: PointOfInterestId
-    arrivals: PointOfInterestId[]
-    numberOfPassengers: NumberOfPassengers
+  origin: PointOfInterestId;
+  arrivals: PointOfInterestId[];
+  numberOfPassengers: NumberOfPassengers;
 }
 
 export class Path extends ValueObject<Props> {
-    static with(
-        origin: PointOfInterestId,
-        arrivals: PointOfInterestId[],
-        numberOfPassengers: NumberOfPassengers,
-    ): Path {
-        return new this({origin, arrivals, numberOfPassengers});
-    }
+  static with(
+    origin: PointOfInterestId,
+    arrivals: PointOfInterestId[],
+    numberOfPassengers: NumberOfPassengers,
+  ): Path {
+    return new this({ origin, arrivals, numberOfPassengers });
+  }
 
-    get origin(): PointOfInterestId {
-        return this.props.origin;
-    }
+  get origin(): PointOfInterestId {
+    return this.props.origin;
+  }
 
-    get arrivals(): PointOfInterestId[] {
-        return this.props.arrivals;
-    }
+  get arrivals(): PointOfInterestId[] {
+    return this.props.arrivals;
+  }
 
-    get numberOfPassengers(): NumberOfPassengers {
-        return this.props.numberOfPassengers;
-    }
+  get numberOfPassengers(): NumberOfPassengers {
+    return this.props.numberOfPassengers;
+  }
 }
