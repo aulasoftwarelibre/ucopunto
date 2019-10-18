a1b23faf923559ba8fd75cf283503290765165c9
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-10-18 18:57:07 +0200

    Added Path VO

---
+++ src/ride-request/domain/model/path.spec.ts
@@ -0,0 +1,21 @@
+import { PointOfInterestId } from '../../../poi/domain/model/poi-id'
+import uuid = require('uuid');
+import { NumberOfPassengers } from './number-of-passengers';
+import { Path } from './path';
+
+describe('Path', () => {
+    const origin: PointOfInterestId = PointOfInterestId.fromString(uuid());
+    const arrivals: PointOfInterestId[] = [
+        PointOfInterestId.fromString(uuid()), 
+        PointOfInterestId.fromString(uuid()),
+        PointOfInterestId.fromString(uuid()),
+    ]
+    const numberOfPassengers: NumberOfPassengers = NumberOfPassengers.fromInteger(2);
+    
+    it('should be constructed correctly', () => {
+        const vo: Path = Path.with(origin, arrivals, numberOfPassengers);
+        expect(vo.origin).toBe(origin);
+        expect(vo.arrivals).toBe(arrivals);
+        expect(vo.numberOfPassengers).toBe(numberOfPassengers);
+    })
+})
