44789452865cf36ed7c2598c7d0dcca2a5d13f76
Pepe <pepemarquezof@gmail.com>
2019-10-18 17:01:07 +0200

    poi-name class and tests created

---
+++ src/poi/domain/model/poi-name.spec.ts
@@ -0,0 +1,16 @@
+import { PointOfInterestName } from "./name";
+
+describe('Poi Name', () => {
+
+  it('should be a string', () => {
+    const vo = PointOfInterestName.fromString("Hola Caracola");
+
+    expect(vo.value).toBe("Hola Caracola");
+  });
+
+  it('should not be a empty string', () => {
+    const t = () => {const vo = PointOfInterestName.fromString("");};
+
+    expect(t).toThrowError();
+  });
+});
---
+++ src/poi/domain/model/poi-name.ts
@@ -0,0 +1,22 @@
+import { generate } from "rxjs"
+import { ValueObject } from "../../../core/domain";
+
+
+interface NameProps {
+    value: string;
+}
+
+export class PointOfInterestName extends ValueObject<NameProps> {
+
+    public static fromString(name: string): PointOfInterestName {
+        if( name.length === 0){
+            throw new Error('Invalid Name');
+        }
+        return new PointOfInterestName({ value: name });
+    }
+
+
+    get value(): string {
+        return this.props.value;
+    }
+}
