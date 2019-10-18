65560c58c5da1c32533a088c45c3e22739086b5d
Adoen Lunnae <unshekdelmonton@gmail.com>
2019-10-18 17:44:46 +0200

    Changed fromString to fromInteger

--- src/ride-request/domain/model/number-of-passengers.spec.ts
+++ src/ride-request/domain/model/number-of-passengers.spec.ts
@@ -3,11 +3,11 @@ import { InvalidNumberOfPassengersException } from '../exceptions'
 
 describe('Number of Passengers', () => {
   it('should be created with the value if it is valid', () => {
-    const vo = NumberOfPassengers.fromString('2');
+    const vo = NumberOfPassengers.fromInteger(2);
     expect(vo.value).toBe(2);
   });
 
   it('should throw an exception if the value is invalid', () => {
-    expect(() => NumberOfPassengers.fromString('-1')).toThrow(InvalidNumberOfPassengersException);
+    expect(() => NumberOfPassengers.fromInteger(-1)).toThrow(InvalidNumberOfPassengersException);
   });
 });
--- src/ride-request/domain/model/number-of-passengers.ts
+++ src/ride-request/domain/model/number-of-passengers.ts
@@ -7,12 +7,11 @@ interface Props {
 }
 
 export class NumberOfPassengers extends ValueObject<Props> {
-  static fromString(value: string): NumberOfPassengers {
-    const nPassengers: number = parseInt(value, 10);
-    if (nPassengers < 1) {
-      throw InvalidNumberOfPassengersException.withNumberOfPassengers(nPassengers);
+  static fromInteger(value: number): NumberOfPassengers {
+    if (value < 1) {
+      throw InvalidNumberOfPassengersException.withNumberOfPassengers(value);
     }
-    return new this({ value: nPassengers });
+    return new this({ value: value });
   }
 
   get value() {
