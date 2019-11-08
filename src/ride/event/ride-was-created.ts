dd89b525542eb2f2439f922050148cb4c08188ec
i62cogag <i62cogag@uco.es>
2019-11-08 17:08:43 +0100

    Ride was created event domain

---
+++ src/ride/event/ride-was-created.ts
@@ -0,0 +1,8 @@
+import { DomainEvent } from '../../core/domain';
+
+export class RideWasCreated implements DomainEvent {
+  constructor(
+    public readonly id: string,
+    public readonly driverId: string,
+  ) {}
+}
