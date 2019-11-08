7f509c6120217c1185324f0cdb2467bee4800401
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-11-08 18:26:28 +0100

    Fixed State Format

--- src/ride-request/domain/exceptions/invalid-ride-request-state.error.ts
+++ src/ride-request/domain/exceptions/invalid-ride-request-state.error.ts
@@ -1,8 +1,5 @@
 export class InvalidRideRequestStateError extends Error {
-    static withRideRequestState(
-      value: string,
-    ): InvalidRideRequestStateError {
-      return new this('${value} is not a valid ride request state');
-    }
+  static withRideRequestState(value: string): InvalidRideRequestStateError {
+    return new this('${value} is not a valid ride request state');
   }
-  
+}
