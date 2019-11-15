eac354d8576458a2293be0d3d3ef0f0d514bd7cb
rexuswolf <francisjmp99@gmail.com>
2019-11-15 19:05:49 +0100

    Added UserIdAlreadyRegistered

--- src/user/domain/model/user-id.spec.ts
+++ src/user/domain/model/user-id.spec.ts
@@ -1,12 +1,22 @@
 import { UserId } from './user-id';
+import { User } from './user';
+import { UserIdAlreadyRegisteredError } from '../exception';
 
 describe('UserId', () => {
   const uuidA = '1061abe8-37e5-4623-8696-a9fd40797f73';
   const uuidB = 'f75dde03-2c8b-4e5a-89a2-ad22d52c18cf';
+  const username = Username.fromString('johndoe');
 
   it('should be an uuid v4', () => {
     const vo = UserId.fromString(uuidA);
 
     expect(vo.value).toBe(uuidA);
+  });
+
+  it('should be unique' , () => {
+    const userId = UserId.fromString(uuidA);
+    const user = User.add(userId, username);
+    
+    expect(() => User.add(userId, username)).toThrow(UserIdAlreadyRegisteredError);
   });
 });
