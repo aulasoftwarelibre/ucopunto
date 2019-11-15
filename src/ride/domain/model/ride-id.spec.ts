91ac164e43f2b4cff66555a492b5230086fa5a6e
rexuswolf <francisjmp99@gmail.com>
2019-11-15 19:19:00 +0100

    Modified VO tests

--- src/ride/domain/model/ride-id.spec.ts
+++ src/ride/domain/model/ride-id.spec.ts
@@ -1,23 +1,12 @@
-import { Ride } from './ride';
 import { RideId } from './ride-id';
-import { UserId } from '../../../user/domain/model/user-id';
-import { RideIdAlreadyRegisteredError } from '../exception';
 
 describe('RideId', () => {
   const uuidA = '1061abe8-37e5-4623-8696-a9fd40797f73';
-  const uuidB = 'f75dde03-2c8b-4e5a-89a2-ad22d52c18cf';
 
   it('should be an uuid v4', () => {
     const vo = RideId.fromString(uuidA);
 
     expect(vo.value).toBe(uuidA);
   });
-
-  it('should be unique' , () => {
-    const rideId = RideId.fromString(uuidA);
-    const driverId = UserId.fromString(uuidB);
-    const ride = Ride.add(rideId, driverId);
-    
-    expect(() => Ride.add(rideId, driverId)).toThrow(RideIdAlreadyRegisteredError);
-  });
 });
+
